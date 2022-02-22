5d7c64ebb8b7a583e14e5c1e62ea5ddc4171eec0
AckonSamuel <ackons433@gmail.com>
2022-02-22 16:30:22 +0000

    Add styles and live server

--- webpack.config.js
+++ webpack.config.js
@@ -5,14 +5,29 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
+    devServer: {
+        static: './dist',
+      },
     plugins: [
         new HtmlWebpackPlugin({
-          title: 'Output Management',
+          template: './src/index.html'
         }),
       ],
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
     },
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },
+          {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+          },
+        ],
+      },
 };
